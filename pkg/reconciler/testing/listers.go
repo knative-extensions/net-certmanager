4c8c74a5428d13565d26f5326d66738350658bf9
Zhimin Xiang <zhiminx@google.com>
2020-04-03 01:53:48 +0000

    user net-certmanager testing listers and factory

---
+++ pkg/reconciler/testing/listers.go
@@ -0,0 +1,128 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package testing
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	fakekubeclientset "k8s.io/client-go/kubernetes/fake"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
+
+	acmelisters "knative.dev/net-certmanager/pkg/client/certmanager/listers/acme/v1alpha2"
+	certmanagerlisters "knative.dev/net-certmanager/pkg/client/certmanager/listers/certmanager/v1alpha2"
+	"knative.dev/pkg/reconciler/testing"
+	networking "knative.dev/serving/pkg/apis/networking/v1alpha1"
+	fakeservingclientset "knative.dev/serving/pkg/client/clientset/versioned/fake"
+	networkinglisters "knative.dev/serving/pkg/client/listers/networking/v1alpha1"
+
+	acmev1alpha2 "github.com/jetstack/cert-manager/pkg/apis/acme/v1alpha2"
+	cmv1alpha2 "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1alpha2"
+)
+
+var clientSetSchemes = []func(*runtime.Scheme) error{
+	fakeservingclientset.AddToScheme,
+	fakekubeclientset.AddToScheme,
+	cmv1alpha2.AddToScheme,
+	acmev1alpha2.AddToScheme,
+}
+
+type Listers struct {
+	sorter testing.ObjectSorter
+}
+
+func NewListers(objs []runtime.Object) Listers {
+	scheme := NewScheme()
+
+	ls := Listers{
+		sorter: testing.NewObjectSorter(scheme),
+	}
+
+	ls.sorter.AddObjects(objs...)
+
+	return ls
+}
+
+func NewScheme() *runtime.Scheme {
+	scheme := runtime.NewScheme()
+
+	for _, addTo := range clientSetSchemes {
+		addTo(scheme)
+	}
+	return scheme
+}
+
+func (*Listers) NewScheme() *runtime.Scheme {
+	return NewScheme()
+}
+
+// IndexerFor returns the indexer for the given object.
+func (l *Listers) IndexerFor(obj runtime.Object) cache.Indexer {
+	return l.sorter.IndexerForObjectType(obj)
+}
+
+func (l *Listers) GetServingObjects() []runtime.Object {
+	return l.sorter.ObjectsForSchemeFunc(fakeservingclientset.AddToScheme)
+}
+
+func (l *Listers) GetKubeObjects() []runtime.Object {
+	return l.sorter.ObjectsForSchemeFunc(fakekubeclientset.AddToScheme)
+}
+
+// GetIngressLister get lister for Ingress resource.
+func (l *Listers) GetIngressLister() networkinglisters.IngressLister {
+	return networkinglisters.NewIngressLister(l.IndexerFor(&networking.Ingress{}))
+}
+
+// GetK8sServiceLister get lister for K8s Service resource.
+func (l *Listers) GetK8sServiceLister() corev1listers.ServiceLister {
+	return corev1listers.NewServiceLister(l.IndexerFor(&corev1.Service{}))
+}
+
+// GetEndpointsLister get lister for K8s Endpoints resource.
+func (l *Listers) GetEndpointsLister() corev1listers.EndpointsLister {
+	return corev1listers.NewEndpointsLister(l.IndexerFor(&corev1.Endpoints{}))
+}
+
+// GetSecretLister get lister for K8s Secret resource.
+func (l *Listers) GetSecretLister() corev1listers.SecretLister {
+	return corev1listers.NewSecretLister(l.IndexerFor(&corev1.Secret{}))
+}
+
+// GetCMCertificateLister gets lister for Cert Manager Certificate resource.
+func (l *Listers) GetCMCertificateLister() certmanagerlisters.CertificateLister {
+	return certmanagerlisters.NewCertificateLister(l.IndexerFor(&cmv1alpha2.Certificate{}))
+}
+
+// GetCMClusterIssuerLister gets lister for Cert Manager ClusterIssuer resource.
+func (l *Listers) GetCMClusterIssuerLister() certmanagerlisters.ClusterIssuerLister {
+	return certmanagerlisters.NewClusterIssuerLister(l.IndexerFor(&cmv1alpha2.ClusterIssuer{}))
+}
+
+// GetCMChallengeLister gets lister for Cert Manager Challenge resource.
+func (l *Listers) GetCMChallengeLister() acmelisters.ChallengeLister {
+	return acmelisters.NewChallengeLister(l.IndexerFor(&acmev1alpha2.Challenge{}))
+}
+
+// GetCMCertificateObjects gets a list of Cert-Manager Certificate objects.
+func (l *Listers) GetCMCertificateObjects() []runtime.Object {
+	return l.sorter.ObjectsForSchemeFunc(cmv1alpha2.AddToScheme)
+}
+
+// GetCertificateLister get lister for Certificate resource.
+func (l *Listers) GetCertificateLister() networkinglisters.CertificateLister {
+	return networkinglisters.NewCertificateLister(l.IndexerFor(&networking.Certificate{}))
+}
