3e7a18a1725e015a10b8b194114f8a6dbc3e1de4
Zhimin Xiang <zhiminx@google.com>
2020-05-04 17:49:44 -0700

    Add webhook for net-certmanager repo (#16)
    * Add webhook for net-certmanager
    
    * fix the licence
    
    * fix typo
    
    * fix the vendor

---
+++ cmd/webhook/main.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	certconfig "knative.dev/net-certmanager/pkg/reconciler/certificate/config"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/injection/sharedmain"
+	"knative.dev/pkg/signals"
+	"knative.dev/pkg/webhook"
+	"knative.dev/pkg/webhook/certificates"
+	"knative.dev/pkg/webhook/configmaps"
+	"knative.dev/pkg/webhook/resourcesemantics"
+)
+
+var types = map[schema.GroupVersionKind]resourcesemantics.GenericCRD{}
+
+func NewConfigValidationController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	return configmaps.NewAdmissionController(ctx,
+
+		// Name of the resource webhook.
+		"config.webhook.net-certmanager.networking.internal.knative.dev",
+
+		// The path on which to serve the webhook.
+		"/config-validation",
+
+		// The configmaps to validate.
+		configmap.Constructors{
+			certconfig.CertManagerConfigName: certconfig.NewCertManagerConfigFromConfigMap,
+		},
+	)
+}
+
+func main() {
+	ctx := webhook.WithOptions(signals.NewContext(), webhook.Options{
+		ServiceName: "net-certmanager-webhook",
+		Port:        8443,
+		SecretName:  "net-certmanager-webhook-certs",
+	})
+
+	sharedmain.WebhookMainWithContext(
+		ctx, "net-certmanager-webhook",
+		certificates.NewController,
+		NewConfigValidationController,
+	)
+}
