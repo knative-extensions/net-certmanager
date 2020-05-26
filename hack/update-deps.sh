311bc26c29ca3e902f7a9f8f4141e5cfdc2ba59e
Markus Thömmes <markusthoemmes@me.com>
2020-05-26 14:00:08 +0200

    [RELEASE] Pin serving to 0.15.

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -29,13 +29,13 @@ export GO111MODULE=on
 export GOFLAGS=-mod=vendor
 
 # This controls the release branch we track.
-VERSION="master"
+VERSION="release-0.15"
 
 # The list of dependencies that we track at HEAD and periodically
 # float forward in this repository.
 FLOATING_DEPS=(
-  "knative.dev/test-infra@release-0.15"
-  "knative.dev/pkg@release-0.15"
+  "knative.dev/test-infra@${VERSION}"
+  "knative.dev/pkg@${VERSION}"
   "knative.dev/serving@${VERSION}"
 )
 
