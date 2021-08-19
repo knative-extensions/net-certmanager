50175905ad5f851b1307191ea0b36acb04938e6e
knative-automation <automation@knative.team>
2021-08-19 10:21:04 -0700

    upgrade to latest dependencies (#271)
    bumping knative.dev/hack e28525d...815cd31:
      > 815cd31 Stop performing pre-releases (# 77)
    bumping knative.dev/pkg 21eb4c1...7b5ecbc:
      > 7b5ecbc Sketch out an alternate way of injecting clients/informers (# 2210)
      > 8c88fa2 Revert "Remove the unused file (# 1935)" (# 2213)
      > 50410e0 Use consistent case for "Deprecated" comments (# 2216)
      > 35bcd16 Drop redundant pointers and decoders (# 2211)
      > bf176d5 Sink two codegen passes below loop. (# 2209)
      > 2c8a7b6 Update community files (# 2208)
      > 23b0147 Update actions (# 2206)
    bumping knative.dev/networking acdfd41...90ed29a:
      > 90ed29a Format go code (# 480)
      > e347271 Use actual cluster domain for TestRewriteHost (# 476)
      > 078ce25 Add a new test case to request unexpected hash (# 468)
      > b245118 Bump Golang version to 1.16 (# 475)
      > d3550bf Update community files (# 478)
      > 8fad814 Update actions (# 477)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- pkg/client/certmanager/injection/informers/certmanager/v1/clusterissuer/clusterissuer.go
+++ pkg/client/certmanager/injection/informers/certmanager/v1/clusterissuer/clusterissuer.go
@@ -21,8 +21,15 @@ package clusterissuer
 import (
 	context "context"
 
+	apiscertmanagerv1 "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	labels "k8s.io/apimachinery/pkg/labels"
+	cache "k8s.io/client-go/tools/cache"
+	versioned "knative.dev/net-certmanager/pkg/client/certmanager/clientset/versioned"
 	v1 "knative.dev/net-certmanager/pkg/client/certmanager/informers/externalversions/certmanager/v1"
+	client "knative.dev/net-certmanager/pkg/client/certmanager/injection/client"
 	factory "knative.dev/net-certmanager/pkg/client/certmanager/injection/informers/factory"
+	certmanagerv1 "knative.dev/net-certmanager/pkg/client/certmanager/listers/certmanager/v1"
 	controller "knative.dev/pkg/controller"
 	injection "knative.dev/pkg/injection"
 	logging "knative.dev/pkg/logging"
@@ -30,6 +37,7 @@ import (
 
 func init() {
 	injection.Default.RegisterInformer(withInformer)
+	injection.Dynamic.RegisterDynamicInformer(withDynamicInformer)
 }
 
 // Key is used for associating the Informer inside the context.Context.
@@ -41,6 +49,11 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 	return context.WithValue(ctx, Key{}, inf), inf.Informer()
 }
 
+func withDynamicInformer(ctx context.Context) context.Context {
+	inf := &wrapper{client: client.Get(ctx)}
+	return context.WithValue(ctx, Key{}, inf)
+}
+
 // Get extracts the typed informer from the context.
 func Get(ctx context.Context) v1.ClusterIssuerInformer {
 	untyped := ctx.Value(Key{})
@@ -49,4 +62,39 @@ func Get(ctx context.Context) v1.ClusterIssuerInformer {
 			"Unable to fetch knative.dev/net-certmanager/pkg/client/certmanager/informers/externalversions/certmanager/v1.ClusterIssuerInformer from context.")
 	}
 	return untyped.(v1.ClusterIssuerInformer)
+}
+
+type wrapper struct {
+	client versioned.Interface
+}
+
+var _ v1.ClusterIssuerInformer = (*wrapper)(nil)
+var _ certmanagerv1.ClusterIssuerLister = (*wrapper)(nil)
+
+func (w *wrapper) Informer() cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(nil, &apiscertmanagerv1.ClusterIssuer{}, 0, nil)
+}
+
+func (w *wrapper) Lister() certmanagerv1.ClusterIssuerLister {
+	return w
+}
+
+func (w *wrapper) List(selector labels.Selector) (ret []*apiscertmanagerv1.ClusterIssuer, err error) {
+	lo, err := w.client.CertmanagerV1().ClusterIssuers().List(context.TODO(), metav1.ListOptions{
+		LabelSelector: selector.String(),
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
+	if err != nil {
+		return nil, err
+	}
+	for idx := range lo.Items {
+		ret = append(ret, &lo.Items[idx])
+	}
+	return ret, nil
+}
+
+func (w *wrapper) Get(name string) (*apiscertmanagerv1.ClusterIssuer, error) {
+	return w.client.CertmanagerV1().ClusterIssuers().Get(context.TODO(), name, metav1.GetOptions{
+		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+	})
 }
